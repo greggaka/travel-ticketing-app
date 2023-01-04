d36aecf0216dfccb79555f0765dd1064b22dc083
greggaka <greggakamine@gmail.com>
2023-01-04 11:05:03 -1000

    first commit - created new spring boot proj

---
+++ travel-ticketing-system/src/main/java/com/project/travelticketingsystem/TravelTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.travelticketingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelTicketingSystemApplication.class, args);
+	}
+
+}
---
+++ travel-ticketing-system/src/test/java/com/project/travelticketingsystem/TravelTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.travelticketingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
